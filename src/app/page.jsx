341d5c7d67ffa5dcfadd57d19ed1526281fb3431
Matheus <90573780+MatheusVRibeiro@users.noreply.github.com>
2025-08-05 22:49:51 -0300

    introdução

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Primeiro exemplo no Next.js</p>
+      <div className="quadrado"></div>
+    </div>
+  );
+}
